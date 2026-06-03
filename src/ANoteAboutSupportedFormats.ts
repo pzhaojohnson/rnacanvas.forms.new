f09a617ce6d1c8cc828ae2853c5d3b3868577d9b
Philip Johnson <p.zhao.johnson@gmail.com>
2026-06-02 20:44:26 -0400

    Edit New form text content

--- src/ANoteAboutSupportedFormats.ts
+++ src/ANoteAboutSupportedFormats.ts
@@ -6,7 +6,7 @@ export class ANoteAboutSupportedFormats {
   constructor() {
     this.domNode.classList.add(styles['text']);
 
-    this.domNode.textContent = 'In general, RNAcanvas tries to support common sequence and structure formats (e.g., RNAfold results page output, mfold CT files, copying and pasting a sequence from GenBank).';
+    this.domNode.textContent = 'In general, RNAcanvas tries to support common sequence and structure formats (e.g., RNAfold results page output, mfold CT files, copying and pasting sequences from GenBank).';
 
     this.domNode.style.marginTop = '37px';
   }
